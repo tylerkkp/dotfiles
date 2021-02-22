92b6f862799321091ba9f86aec10bbfe8f4c2273
Tyler K Kuromiya Parker <tylerkkp@gmail.com>
2021-02-22 14:06:23 -0800

    add my_aliases.zsh

---
+++ my_aliases.zsh
@@ -0,0 +1,15 @@
+# navigation
+alias ..="cd .."
+alias dev="cd /Users/tylerkp/Documents/dev"
+
+# python3
+alias p="python3"
+
+# open Markdown files with VS Code Insiders
+alias -s md=code-insiders
+
+# open Python3 files with VS Code Insiders
+alias -s py=code-insiders
+
+# open Rust files with VS Code Insiders
+alias -s rs=code-insiders
